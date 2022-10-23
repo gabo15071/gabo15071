03b226497de24bd3370d9de9dafd9287cce0b1d6
gabo15071 <116222129+gabo15071@users.noreply.github.com>
2022-10-22 20:21:53 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('lo sabia xd ')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('click',function () {
+    alert('sabía que no yo tampoco te quiero xd ')
+});
